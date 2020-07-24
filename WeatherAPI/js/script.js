23fe2e17cad46a2c40d0b272bf35364b4d346811
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-07-23 17:08:17 -0700

    Add files via upload

---
+++ WeatherAPI/js/script.js
@@ -0,0 +1,20 @@
+/*var city = "Paris";*/
+
+var city = prompt("Please enter a city", "San Francisco");
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=dc9395065ccb80beb2c407ca184c8826", 
+function(data) {
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = Math.floor(data.main.temp);
+    var weather = data.weather[0].main
+
+    $(".icon").attr("src", icon);  
+    $(".weather").append(weather);
+    $(".temp").append(temp + "\xB0F");
+    if (city != null) {
+        document.getElementById("demo").innerHTML = "Today in " + city + ". It is: ";
+    }
+
+});
